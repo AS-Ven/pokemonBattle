42cbd088379cd1b0d7830c0e026ffb8786dc81b4
Mila Paounov <paounovm@gmail.com>
2024-12-01 17:23:51 +0100

    feat: css

--- app/views/Layout.php
+++ app/views/Layout.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?? 'Mon titre par défaut' ?></title>
-    <!-- <link rel="stylesheet" href="/assets/style.css"> -->
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
     <header>
         <h1>Mon site MVC</h1>
         <nav>
-          <a href="/pokemon/findAll">Liste des utilisateurs</a>
+            <a href="/pokemon/findAll">Liste des utilisateurs</a>
         </nav>
     </header>
     
